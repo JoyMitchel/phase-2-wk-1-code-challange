ad094db72355964326b14bf198970e13f0fc0a59
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-04-14 01:10:56 +0300

    expense table

---
+++ src/components/ExpenseTable.jsx
@@ -0,0 +1,24 @@
+function ExpenseTable({ expenses }) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense, index) => (
+            <tr key={index}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+  export default ExpenseTable;
+  
